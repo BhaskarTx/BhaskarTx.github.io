d8330d6607cdfa7d36e46a48528711be216b47a5
Bhaskar Agrawal <8dbdown@gmail.com>
2021-12-09 07:08:08 +0530

    i-o done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
+var txtOutput = document.querySelector("#txt-output");
 function clickEvent(){
-        console.log("clicked");
-        console.log("input is - > ", txtInput.value);
+        // console.log("clicked");
+        // console.log("input is - > ", txtInput.value);
+        txtOutput.innerText = "translated "+ txtInput.value;
 }
 
 btnTrans.addEventListener("click", clickEvent);
