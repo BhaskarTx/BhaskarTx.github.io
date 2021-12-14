5a9b99909ea335359c5790235f95c4400a261a79
Bhaskar Agrawal <8dbdown@gmail.com>
2021-12-14 08:01:38 +0530

    Update: api call

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
-function clickEvent(){
+var serverURL = "https://api.funtranslations.com/translate/"
+
+function getTranslateURL(text) {
+        return serverURL + "?text=" + text
+}
+
+
+function clickEvent() {
         // console.log("clicked");
         // console.log("input is - > ", txtInput.value);
-        txtOutput.innerText = "translated "+ txtInput.value;
+        // txtOutput.innerText = "translated "+ txtInput.value;
+        var inputText = txtInput.value;
+        fetch(getTranslateURL(inputText))
+                .then(response => response.json)
+                .then(json => console.log(json))
+
 }
 
 btnTrans.addEventListener("click", clickEvent);
