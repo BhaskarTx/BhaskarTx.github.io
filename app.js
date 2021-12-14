0f3ffcdf2cd337044b4053de628b22c7ddb81814
Bhaskar Agrawal <8dbdown@gmail.com>
2021-12-14 09:28:20 +0530

    completed functionality

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
-var serverURL = "https://api.funtranslations.com/translate/"
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
 
 function getTranslateURL(text) {
         return serverURL + "?text=" + text
 }
 
+function errorHandler(error) {
+        console.log("error occured", error);
+        alert("something wrong with server ! try again later");
+}
 
 function clickEvent() {
         // console.log("clicked");
@@ -14,8 +21,18 @@ function clickEvent() {
         // txtOutput.innerText = "translated "+ txtInput.value;
         var inputText = txtInput.value;
         fetch(getTranslateURL(inputText))
-                .then(response => response.json)
-                .then(json => console.log(json))
+                .then(response => response.json())
+                .then(json => {
+                                var translatedText = json.contents.translated;
+                                txtOutput.textContent = translatedText;
+                        }
+
+
+                        // txtOutput.textContent(json.contents.translated)
+                )
+                // .then(json => console.log(json.contents.translated))
+
+                .catch(errorHandler);
 
 }
 
